76e5cc166243a63ea8df9a477ff4a717e2b94b62
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2023-01-04 12:39:59 +0000

    PSR-34 - use bootstrap ip allowlist

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.22.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.11.0-play-28",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "6.4.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % "7.11.0",
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.68.0"
   )
 
