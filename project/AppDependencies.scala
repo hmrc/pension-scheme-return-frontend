c92da08b233bc3d9b9d5666ffaa1d4631a9b923d
Kurt Logan <kurt.logan@digital.hmrc.gov.uk>
2023-01-23 16:38:30 +0000

    PSR-44 - upgraded deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,13 +4,14 @@ object AppDependencies {
   import play.core.PlayVersion
 
   private val bootstrapVersion = "7.12.0"
+  private val hmrcMongoVersion = "0.74.0"
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"             % "3.22.0-play-28",
     "uk.gov.hmrc"       %% "play-conditional-form-mapping"  % "1.12.0-play-28",
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28"     % bootstrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % "0.74.0"
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"             % hmrcMongoVersion
   )
 
   val test = Seq(
@@ -23,7 +24,7 @@ object AppDependencies {
     "com.typesafe.play"       %% "play-test"               % PlayVersion.current,
     "org.mockito"             %% "mockito-scala"           % "1.16.42",
     "org.scalacheck"          %% "scalacheck"              % "1.15.4",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % "0.68.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28" % hmrcMongoVersion,
     "com.vladsch.flexmark"    %  "flexmark-all"            % "0.62.2"
   ).map(_ % "test, it")
 
