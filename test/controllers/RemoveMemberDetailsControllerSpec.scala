ad8b7ae8b0023e309c54c854dd6bc308f51e4686
narendrapaduchuri <narendra.paduchuri@digital.hmrc.gov.uk>
2023-03-23 13:19:46 +0000

    PSR-116 test cases and messages

--- test/controllers/RemoveMemberDetailsControllerSpec.scala
+++ test/controllers/RemoveMemberDetailsControllerSpec.scala
@@ -1,7 +1,23 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package controllers
 
 import controllers.RemoveMemberDetailsController.{form, viewModel}
-import eu.timepit.refined.refineMV
+import eu.timepit.refined.{refineMV, refineV}
 import forms.YesNoPageFormProvider
 import models.{CheckMode, NameDOB, NormalMode}
 import pages.{AccountingPeriodPage, MemberDetailsPage}
@@ -13,9 +29,7 @@ class RemoveMemberDetailsControllerSpec extends ControllerBaseSpec {
   private lazy val onPageLoad = routes.RemoveMemberDetailsController.onPageLoad(srn, refineMV(1), CheckMode)
   private lazy val onSubmit = routes.RemoveMemberDetailsController.onSubmit(srn, refineMV(1), CheckMode)
 
-  val memberDetails = nameDobGen.sample.value
-
-//  private val memberDetails = NameDOB("testFirstname", "testLastName", LocalDate.of(2020, 12, 12))
+  override val memberDetails = NameDOB("testFirstname", "testLastName", LocalDate.of(2020, 12, 12))
 
   private val userAnswers = defaultUserAnswers
     .set(MemberDetailsPage(srn, refineMV(1)), memberDetails)
@@ -32,11 +46,16 @@ class RemoveMemberDetailsControllerSpec extends ControllerBaseSpec {
 
       view(
         form(injected[YesNoPageFormProvider]),
-        viewModel(srn, refineMV(1), memberDetails, NormalMode)
+        viewModel(srn, refineMV(1), memberDetails, CheckMode)
       )
     })
 
-    behave.like(redirectToPage(onPageLoad, controllers.routes.JourneyRecoveryController.onPageLoad()))
+    behave.like(
+      redirectToPage(
+        onPageLoad,
+        controllers.routes.JourneyRecoveryController.onPageLoad()
+      )
+    )
 
     behave.like(journeyRecoveryPage("onPageLoad", onPageLoad))
 
