ae6577d243c89569213137f3db5e744fb19db356
Kurt Logan <4375059+kurtlogan@users.noreply.github.com>
2023-04-19 11:22:45 +0100

    PSR-141 | added land or property held page (#89)

--- app/navigation/nonsipp/NonSippNavigator.scala
+++ app/navigation/nonsipp/NonSippNavigator.scala
@@ -79,6 +79,7 @@ class NonSippNavigator @Inject()() extends Navigator {
       PersonalContributionsNavigator,
       ReceiveTransferNavigator,
       TransferOutNavigator,
+      LandOrPropertyNavigator,
       LoanMadeOrOutstandingNavigator,
       PensionCommencementLumpSumNavigator,
       SharesInSponsoringEmployerNavigator,
