7f29b3a7a3db3dfc19cb56495ca34ce6360a38a6
Kornel Berec <26416940+kberec@users.noreply.github.com>
2023-06-13 16:18:22 +0100

    PSR-236 Added test

--- app/controllers/nonsipp/memberdetails/UploadMemberDetailsController.scala
+++ app/controllers/nonsipp/memberdetails/UploadMemberDetailsController.scala
@@ -52,9 +52,9 @@ class UploadMemberDetailsController @Inject()(
     controllers.routes.UploadCallbackController.callback.absoluteURL(secure = config.secureUpscanCallBack)
 
   def onPageLoad(srn: Srn, mode: Mode): Action[AnyContent] = identifyAndRequireData(srn).async { implicit request =>
-    val tag = "upload-member-details"
-    val successRedirectUrl = config.urls.upscan.successEndpoint.format(srn.value, tag)
-    val failureRedirectUrl = config.urls.upscan.failureEndpoint.format(srn.value, tag)
+    val redirectTag = "upload-member-details"
+    val successRedirectUrl = config.urls.upscan.successEndpoint.format(srn.value, redirectTag)
+    val failureRedirectUrl = config.urls.upscan.failureEndpoint.format(srn.value, redirectTag)
 
     val uploadKey = UploadKey.fromRequest(srn)
 
