439dea8819c7bc1e93a3f762de8da68cb117e17b
alessandrocinque <alessandro5cinque@gmail.com>
2023-10-10 14:43:10 +0100

    PSR-349 initial commit

---
+++ app/controllers/nonsipp/moneyborrowed/WhenBorrowedController.scala
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.nonsipp.moneyborrowed
+
+import config.Constants
+import config.Refined.Max5000
+import controllers.PSRController
+import controllers.actions.IdentifyAndRequireData
+import forms.DatePageFormProvider
+import forms.mappings.errors.DateFormErrors
+import models.Mode
+import models.SchemeId.Srn
+import models.requests.DataRequest
+import navigation.Navigator
+import pages.nonsipp.moneyborrowed.{BorrowedAmountAndRatePage, WhenBorrowedPage}
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.{SaveService, SchemeDateService}
+import viewmodels.DisplayMessage.Message
+import viewmodels.models.{DatePageViewModel, FormPageViewModel}
+import views.html.DatePageView
+import viewmodels.implicits._
+
+import java.time.LocalDate
+import java.time.format.{DateTimeFormatter, FormatStyle}
+import javax.inject.{Inject, Named}
+import scala.concurrent.{ExecutionContext, Future}
+
+class WhenBorrowedController @Inject()(
+  override val messagesApi: MessagesApi,
+  saveService: SaveService,
+  @Named("non-sipp") navigator: Navigator,
+  identifyAndRequireData: IdentifyAndRequireData,
+  formProvider: DatePageFormProvider,
+  schemeDateService: SchemeDateService,
+  val controllerComponents: MessagesControllerComponents,
+  view: DatePageView
+)(implicit ec: ExecutionContext)
+    extends PSRController
+    with I18nSupport {
+
+  private val form =
+    (date: LocalDate, request: DataRequest[AnyContent]) =>
+      WhenBorrowedController.form(formProvider)(date, request.messages(messagesApi))
+
+  def onPageLoad(srn: Srn, index: Max5000, mode: Mode): Action[AnyContent] = identifyAndRequireData(srn) {
+    implicit request =>
+      schemeDateService.taxYearOrAccountingPeriods(srn).merge.getOrRecoverJourney { date =>
+        request.userAnswers.get(BorrowedAmountAndRatePage(srn, index)).getOrRecoverJourney { amountBorrowed =>
+          val preparedForm = {
+            request.userAnswers.fillForm(WhenBorrowedPage(srn, index), form(date.to, request))
+          }
+          Ok(
+            view(
+              preparedForm,
+              WhenBorrowedController
+                .viewModel(srn, index, mode, request.schemeDetails.schemeName, "CHANGE ME!!!", amountBorrowed._1.value)
+            )
+          )
+        }
+      }
+  }
+
+  def onSubmit(srn: Srn, index: Max5000, mode: Mode): Action[AnyContent] = identifyAndRequireData(srn).async {
+    implicit request =>
+      schemeDateService.taxYearOrAccountingPeriods(srn).merge.getOrRecoverJourney { date =>
+        request.userAnswers.get(BorrowedAmountAndRatePage(srn, index)).getOrRecoverJourney { amountBorrowed =>
+          form(date.to, request)
+            .bindFromRequest()
+            .fold(
+              formWithErrors =>
+                Future.successful(
+                  BadRequest(
+                    view(
+                      formWithErrors,
+                      WhenBorrowedController.viewModel(
+                        srn,
+                        index,
+                        mode,
+                        request.schemeDetails.schemeName,
+                        "CHANGE ME!!!",
+                        amountBorrowed._1.value
+                      )
+                    )
+                  )
+                ),
+              value =>
+                for {
+                  updatedAnswers <- Future
+                    .fromTry(request.userAnswers.set(WhenBorrowedPage(srn, index), value))
+                  _ <- saveService.save(updatedAnswers)
+                } yield Redirect(
+                  navigator.nextPage(WhenBorrowedPage(srn, index), mode, updatedAnswers)
+                )
+            )
+        }
+      }
+  }
+}
+
+object WhenBorrowedController {
+  def form(formProvider: DatePageFormProvider)(date: LocalDate, messages: Messages): Form[LocalDate] = formProvider(
+    DateFormErrors(
+      required = "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.required.all",
+      requiredDay = "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.required.day",
+      requiredMonth = "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.required.month",
+      requiredYear = "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.required.year",
+      requiredTwo = "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.required.two",
+      invalidDate = "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.invalid.date",
+      invalidCharacters = "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.invalid.characters",
+      validators = List(
+        DateFormErrors
+          .failIfDateAfter(date, messages("whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.future", date)),
+        DateFormErrors
+          .failIfDateBefore(
+            Constants.earliestDate,
+            messages(
+              "whenDidSchemeAcquireLandOrProperty.dateOfAcquired.error.after",
+              Constants.earliestDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG))
+            )
+          )
+      )
+    )
+  )
+
+  def viewModel(
+    srn: Srn,
+    index: Max5000,
+    mode: Mode,
+    schemeName: String,
+    lenderName: String,
+    amountBorrowed: Double
+  ): FormPageViewModel[DatePageViewModel] =
+    FormPageViewModel(
+      "moneyBorrowed.WhenBorrowed.title",
+      Message("moneyBorrowed.WhenBorrowed.heading", schemeName, lenderName, amountBorrowed.toString),
+      DatePageViewModel(),
+      controllers.nonsipp.moneyborrowed.routes.WhenBorrowedController.onSubmit(srn, index, mode)
+    )
+}
--- app/navigation/nonsipp/MoneyBorrowedNavigator.scala
+++ app/navigation/nonsipp/MoneyBorrowedNavigator.scala
@@ -36,6 +36,9 @@ object MoneyBorrowedNavigator extends JourneyNavigator {
       controllers.routes.UnauthorisedController.onPageLoad()
 
     case BorrowedAmountAndRatePage(srn, index) =>
+      controllers.nonsipp.moneyborrowed.routes.WhenBorrowedController.onPageLoad(srn, index, NormalMode)
+
+    case WhenBorrowedPage(srn, index) =>
       controllers.routes.UnauthorisedController.onPageLoad()
   }
 
