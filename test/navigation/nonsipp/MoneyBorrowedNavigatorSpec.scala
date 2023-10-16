229cabb1f72a9d719f0c60b35d8109bfee228938
narendrapaduchuri <115544611+narendrapaduchuri@users.noreply.github.com>
2023-10-16 13:32:43 +0100

    Update MoneyBorrowedNavigatorSpec.scala
    nav spec

--- test/navigation/nonsipp/MoneyBorrowedNavigatorSpec.scala
+++ test/navigation/nonsipp/MoneyBorrowedNavigatorSpec.scala
@@ -111,7 +111,7 @@ class MoneyBorrowedNavigatorSpec extends BaseSpec with NavigatorBehaviours {
         .navigateToWithIndex(
           index,
           ValueOfSchemeAssetsWhenMoneyBorrowedPage,
-          (srn, _: Max5000, _) => controllers.routes.UnauthorisedController.onPageLoad()
+          (srn, _: Max5000, _) => controllers.nonsipp.moneyborrowed.routes.WhySchemeBorrowedMoneyController.onPageLoad(srn, index, NormalMode)
         )
         .withName("go from value of scheme assets when money borrowed page to unauthorised controller")
     )
