eefa65b3e52bbb1725e57f2c440da50f011aecb5
alessandrocinque <alessandro5cinque@gmail.com>
2023-11-27 15:33:22 +0000

    PSR-399 very rough implementation

---
+++ app/controllers/nonsipp/memberpayments/TransferringSchemeType.scala
@@ -0,0 +1,126 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.nonsipp.memberpayments
+
+import config.Refined.{Max300, Max50, Max5000}
+import controllers.actions.IdentifyAndRequireData
+import forms.{RadioListFormProvider, TextFormProvider}
+import models.SchemeId.Srn
+import models.{Mode, PensionSchemeType, UserAnswers}
+import navigation.Navigator
+import pages.nonsipp.memberpayments.PensionSchemeTypePage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.SaveService
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.FormUtils.FormOps
+import viewmodels.DisplayMessage.Message
+import viewmodels.models.{FormPageViewModel, RadioListRowViewModel, RadioListViewModel}
+import views.html.RadioListView
+import viewmodels.implicits._
+
+import javax.inject.{Inject, Named}
+import scala.concurrent.{ExecutionContext, Future}
+
+class TransferringSchemeType @Inject()(
+  override val messagesApi: MessagesApi,
+  @Named("non-sipp") navigator: Navigator,
+  identifyAndRequireData: IdentifyAndRequireData,
+  formProvider: TextFormProvider,
+  saveService: SaveService,
+  val controllerComponents: MessagesControllerComponents,
+  view: RadioListView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(
+    srn: Srn,
+    index: Max300,
+    secondaryIndex: Max50,
+    mode: Mode
+  ): Action[AnyContent] = identifyAndRequireData(srn) { implicit request =>
+    val form = TransferringSchemeType.form(formProvider)
+    val schemeName = request.schemeDetails.schemeName
+    Ok(
+      view(
+        form.fromUserAnswers(PensionSchemeTypePage(srn, index, secondaryIndex)),
+        TransferringSchemeType.viewModel(srn, index, secondaryIndex, schemeName, mode)
+      )
+    )
+  }
+
+  def onSubmit(
+    srn: Srn,
+    index: Max300,
+    secondaryIndex: Max50,
+    mode: Mode
+  ): Action[AnyContent] = identifyAndRequireData(srn).async { implicit request =>
+    val form = TransferringSchemeType.form(formProvider)
+    val schemeName = request.schemeDetails.schemeName
+    form
+      .bindFromRequest()
+      .fold(
+        formWithErrors =>
+          Future
+            .successful(
+              BadRequest(
+                view(formWithErrors, TransferringSchemeType.viewModel(srn, index, secondaryIndex, schemeName, mode))
+              )
+            ),
+        answer => {
+          for {
+            updatedAnswers <- Future
+              .fromTry(request.userAnswers.set(PensionSchemeTypePage(srn, index, secondaryIndex), answer))
+            _ <- saveService.save(updatedAnswers)
+          } yield Redirect(navigator.nextPage(PensionSchemeTypePage(srn, index, secondaryIndex), mode, updatedAnswers))
+        }
+      )
+  }
+}
+
+object TransferringSchemeType {
+
+  def form(formProvider: TextFormProvider): Form[String] = formProvider.text(
+    "companySellerName.error.required",
+    "companySellerName.error.tooLong",
+    "companySellerName.error.invalid"
+  )
+
+  private def radioListItems(): List[RadioListRowViewModel] =
+    PensionSchemeType.values.map { aType =>
+      RadioListRowViewModel(Message(s"transferring.pensionType.${aType.name}"), aType.name)
+    }
+
+  def viewModel(
+    srn: Srn,
+    index: Max300,
+    secondaryIndex: Max50,
+    schemeName: String,
+    mode: Mode
+  ): FormPageViewModel[RadioListViewModel] =
+    FormPageViewModel(
+      Message("transferring.pensionType.title"),
+      Message("transferring.pensionType.heading", schemeName),
+      RadioListViewModel(
+        None,
+        radioListItems()
+      ),
+      controllers.nonsipp.memberpayments.routes.TransferringSchemeType.onPageLoad(srn, index, secondaryIndex, mode)
+    )
+}
---
+++ app/models/PensionSchemeType.scala
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.libs.json.{JsString, Writes}
+import play.api.mvc.JavascriptLiteral
+import utils.WithName
+
+sealed trait PensionSchemeType {
+  val name: String
+
+  def fold[A](aRegisteredPS: => A, aQualifyingRecognisedOverseasPS: => A, other: => A): A = this match {
+    case PensionSchemeType.RegisteredPS => aRegisteredPS
+    case PensionSchemeType.QualifyingRecognisedOverseasPS => aQualifyingRecognisedOverseasPS
+    case PensionSchemeType.Other => other
+
+  }
+}
+
+object PensionSchemeType extends Enumerable.Implicits {
+
+  case object RegisteredPS extends WithName("registeredPS") with PensionSchemeType
+  case object QualifyingRecognisedOverseasPS extends WithName("qualifyingRecognisedOverseasPS") with PensionSchemeType
+  case object Other extends WithName("other") with PensionSchemeType
+
+  val values: List[PensionSchemeType] = List(RegisteredPS, QualifyingRecognisedOverseasPS, Other)
+
+  implicit val enumerable: Enumerable[PensionSchemeType] = Enumerable(values.map(v => (v.toString, v)): _*)
+
+  implicit val jsLiteral: JavascriptLiteral[PensionSchemeType] = (value: PensionSchemeType) => value.name
+
+  implicit val writes: Writes[PensionSchemeType] =
+    Writes(value => JsString(value.toString))
+}
