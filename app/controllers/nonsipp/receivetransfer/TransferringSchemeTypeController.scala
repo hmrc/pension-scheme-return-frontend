84e08f441cf9c4dc8549bbad20a90c84b52baee2
westwater <gmwestwater@hotmail.co.uk>
2023-12-04 12:55:23 +0000

    wip

--- app/controllers/nonsipp/receivetransfer/TransferringSchemeTypeController.scala
+++ app/controllers/nonsipp/receivetransfer/TransferringSchemeTypeController.scala
@@ -128,7 +128,7 @@ object TransferringSchemeTypeController {
       }
     )
 
-  private def formErrors(typeName: String) =
+  private def formErrors(typeName: String): InputFormErrors =
     if (typeName == PensionSchemeType.RegisteredPS.name) {
       InputFormErrors.inputPSTRandQROPS(
         "transferring.conditional.PSTR.error.required",
