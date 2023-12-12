4eaea9700d8acc9764ac825e64f5cbd02c32b60e
Alessandro Cinque <alessandro5cinque@gmail.com>
2023-12-12 10:52:03 +0000

    PSR-515 Page completed (#414)
    * PSR-515 Transferout scheme type

---
+++ test/controllers/nonsipp/membertransferout/ReceivingSchemeTypeControllerSpec.scala
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.nonsipp.membertransferout
+
+import config.Refined.{Max300, Max5}
+import controllers.ControllerBaseSpec
+import eu.timepit.refined.refineMV
+import forms.RadioListFormProvider
+import models.NormalMode
+import pages.nonsipp.membertransferout.ReceivingSchemeNamePage
+import views.html.RadioListView
+
+class ReceivingSchemeTypeControllerSpec extends ControllerBaseSpec {
+
+  private val index = refineMV[Max300.Refined](1)
+  private val secondarylIndex = refineMV[Max5.Refined](1)
+
+  private lazy val onPageLoad =
+    controllers.nonsipp.membertransferout.routes.ReceivingSchemeTypeController
+      .onPageLoad(srn, index, secondarylIndex, NormalMode)
+  private lazy val onSubmit = controllers.nonsipp.membertransferout.routes.ReceivingSchemeTypeController
+    .onSubmit(srn, index, secondarylIndex, NormalMode)
+  private val userAnswers = defaultUserAnswers
+    .unsafeSet(ReceivingSchemeNamePage(srn, index, secondarylIndex), individualName)
+
+  "ReceivingSchemeTypeController" - {
+
+    act.like(renderView(onPageLoad, userAnswers) { implicit app => implicit request =>
+      injected[RadioListView]
+        .apply(
+          ReceivingSchemeTypeController.form(injected[RadioListFormProvider]),
+          ReceivingSchemeTypeController.viewModel(srn, index, secondarylIndex, individualName, NormalMode)
+        )
+    })
+
+    act.like(redirectNextPage(onSubmit, "value" -> "other", "other-conditional" -> "details"))
+
+    act.like(journeyRecoveryPage(onPageLoad).updateName("onPageLoad" + _))
+
+    act.like(saveAndContinue(onSubmit, "value" -> "other", "other-conditional" -> "details"))
+
+    act.like(invalidForm(onSubmit))
+    act.like(journeyRecoveryPage(onSubmit).updateName("onSubmit" + _))
+  }
+}
