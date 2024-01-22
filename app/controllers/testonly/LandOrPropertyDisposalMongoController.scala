d2cca480ae50cc46043e73a1c55da4359254b463
westwater <gmwestwater@hotmail.co.uk>
2024-01-22 09:31:17 +0000

    Fixed disposals test endpoint

--- app/controllers/testonly/LandOrPropertyDisposalMongoController.scala
+++ app/controllers/testonly/LandOrPropertyDisposalMongoController.scala
@@ -16,89 +16,55 @@
 
 package controllers.testonly
 
-import cats.implicits.{toBifunctorOps, toTraverseOps}
 import config.Refined.{Max50, Max5000}
 import controllers.actions.IdentifyAndRequireData
 import eu.timepit.refined._
+import models.HowDisposed.HowDisposed
 import models.SchemeId.Srn
-import models.{RecipientDetails, UserAnswers}
-import pages.nonsipp.landorpropertydisposal.{
-  LandOrPropertyDisposalBuyerConnectedPartyPage,
-  LandOrPropertyStillHeldPage,
-  LandPropertyDisposalCompletedPage,
-  OtherBuyerDetailsPage
-}
-import play.api.i18n.I18nSupport
-import play.api.libs.json.Json
-import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import models.{HowDisposed, IdentityType, Money, RecipientDetails}
+import pages.nonsipp.landorpropertydisposal._
+import play.api.mvc.MessagesControllerComponents
 import services.SaveService
-import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import shapeless._
 import viewmodels.models.SectionCompleted
 
+import java.time.LocalDate
 import javax.inject.Inject
-import scala.concurrent.{ExecutionContext, Future}
-import scala.util.Try
+import scala.concurrent.ExecutionContext
 
 class LandOrPropertyDisposalMongoController @Inject()(
-  saveService: SaveService,
-  identifyAndRequireData: IdentifyAndRequireData,
+  val saveService: SaveService,
+  val identifyAndRequireData: IdentifyAndRequireData,
   val controllerComponents: MessagesControllerComponents
-)(implicit ec: ExecutionContext)
-    extends FrontendBaseController
-    with I18nSupport {
+)(implicit val ec: ExecutionContext)
+    extends TestDataController[Max5000.Refined, Max50.Refined] {
 
-  private val max: Max50 = refineMV(50)
+  override type Pages =
+    PageWithValue[RecipientDetails] ::
+      PageWithValue[Boolean] ::
+      PageWithValue[HowDisposed] ::
+      PageWithValue[Boolean] ::
+      PageWithValue[Money] ::
+      PageWithValue[Boolean] ::
+      PageWithValue[IdentityType] ::
+      PageWithValue[LocalDate] ::
+      PageWithValue[SectionCompleted.type] ::
+      HNil
 
-  def addLandOrPropertyDisposal(srn: Srn, index: Max5000, num: Max50): Action[AnyContent] =
-    identifyAndRequireData(srn).async { implicit request =>
-      for {
-        removedUserAnswers <- Future.fromTry(removeAllLandOrPropertyDisposals(srn, index, request.userAnswers))
-        updatedUserAnswers <- Future.fromTry(
-          updateUserAnswersWithLandOrProperties(num.value, srn, index, removedUserAnswers)
-        )
-        _ <- saveService.save(updatedUserAnswers)
-      } yield Ok(
-        s"Added ${num.value} land or property disposals to UserAnswers for land or property index ${index.value}\n${Json
-          .prettyPrint(updatedUserAnswers.data.decryptedValue)}"
-      )
-    }
+  override val max: Max50 = refineMV(50)
 
-  private def buildIndexes(num: Int): Try[List[Max50]] =
-    (1 to num).map(i => refineV[Max50.Refined](i).leftMap(new Exception(_)).toTry).toList.sequence
-
-  private def removeAllLandOrPropertyDisposals(srn: Srn, index: Max5000, userAnswers: UserAnswers): Try[UserAnswers] =
-    for {
-      indexes <- buildIndexes(max.value)
-      updatedUserAnswers <- indexes.foldLeft(Try(userAnswers)) {
-        case (ua, disposalIndex) =>
-          ua.flatMap(_.remove(LandOrPropertyStillHeldPage(srn, index, disposalIndex)))
-            .flatMap(_.remove(OtherBuyerDetailsPage(srn, index, disposalIndex)))
-      }
-    } yield updatedUserAnswers
-
-  private val recipientDetails = RecipientDetails("test name", "test description")
-
-  private def updateUserAnswersWithLandOrProperties(
-    num: Int,
-    srn: Srn,
-    index: Max5000,
-    userAnswers: UserAnswers
-  ): Try[UserAnswers] =
-    for {
-      indexes <- buildIndexes(num)
-      otherBuyer = indexes.map(disposalIndex => OtherBuyerDetailsPage(srn, index, disposalIndex) -> recipientDetails)
-      stillHeld = indexes.map(disposalIndex => LandOrPropertyStillHeldPage(srn, index, disposalIndex) -> true)
-      connectedParty = indexes.map(
-        disposalIndex => LandOrPropertyDisposalBuyerConnectedPartyPage(srn, index, disposalIndex) -> true
-      )
-      sectionCompleted = indexes.map(
-        disposalIndex => LandPropertyDisposalCompletedPage(srn, index, disposalIndex) -> SectionCompleted
-      )
-      ua1 <- otherBuyer.foldLeft(Try(userAnswers)) {
-        case (ua, (page, value)) => ua.flatMap(_.set(page, value))
-      }
-      ua2 <- stillHeld.foldLeft(Try(ua1)) { case (ua, (page, value)) => ua.flatMap(_.set(page, value)) }
-      ua3 <- connectedParty.foldLeft(Try(ua2)) { case (ua, (page, value)) => ua.flatMap(_.set(page, value)) }
-      ua4 <- sectionCompleted.foldLeft(Try(ua3)) { case (ua, (page, value)) => ua.flatMap(_.set(page, value)) }
-    } yield ua4
+  override def pages(srn: Srn, index: Max5000, secondaryIndex: Max50): Pages = HList(
+    PageWithValue(
+      OtherBuyerDetailsPage(srn, index, secondaryIndex),
+      RecipientDetails("test recipient", "test description")
+    ),
+    PageWithValue(LandOrPropertyStillHeldPage(srn, index, secondaryIndex), true),
+    PageWithValue(HowWasPropertyDisposedOfPage(srn, index, secondaryIndex), HowDisposed.Sold),
+    PageWithValue(LandOrPropertyDisposalBuyerConnectedPartyPage(srn, index, secondaryIndex), true),
+    PageWithValue(TotalProceedsSaleLandPropertyPage(srn, index, secondaryIndex), Money(123.45)),
+    PageWithValue(DisposalIndependentValuationPage(srn, index, secondaryIndex), true),
+    PageWithValue(WhoPurchasedLandOrPropertyPage(srn, index, secondaryIndex), IdentityType.Other),
+    PageWithValue(WhenWasPropertySoldPage(srn, index, secondaryIndex), LocalDate.of(2024, 12, 12)),
+    PageWithValue(LandPropertyDisposalCompletedPage(srn, index, secondaryIndex), SectionCompleted)
+  )
 }
