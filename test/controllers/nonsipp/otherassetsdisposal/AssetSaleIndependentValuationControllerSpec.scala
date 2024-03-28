e01b9c0ffee7e8d643b44f68c464dacfa8183f33
narendrapaduchuri <narendra.paduchuri@digital.hmrc.gov.uk>
2024-03-28 16:40:14 +0000

    PSR-952 sales independent valuation page

---
+++ test/controllers/nonsipp/otherassetsdisposal/AssetSaleIndependentValuationControllerSpec.scala
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.nonsipp.otherassetsdisposal
+
+import pages.nonsipp.otherassetsdisposal.AssetSaleIndependentValuationPage
+import controllers.nonsipp.otherassetsdisposal.AssetSaleIndependentValuationController._
+import config.Refined._
+import controllers.ControllerBaseSpec
+import views.html.YesNoPageView
+import eu.timepit.refined.refineMV
+import forms.YesNoPageFormProvider
+import models.NormalMode
+
+class AssetSaleIndependentValuationControllerSpec extends ControllerBaseSpec {
+
+  private val assetIndex = refineMV[Max5000.Refined](1)
+  private val disposalIndex = refineMV[Max50.Refined](1)
+  private lazy val onPageLoad =
+    routes.AssetSaleIndependentValuationController.onPageLoad(srn, assetIndex, disposalIndex, NormalMode)
+  private lazy val onSubmit =
+    routes.AssetSaleIndependentValuationController.onSubmit(srn, assetIndex, disposalIndex, NormalMode)
+
+//  private val userAnswers = defaultUserAnswers.unsafeSet(CompanyNameRelatedSharesPage(srn, index), companyName)
+
+  "AssetSaleIndependentValuationController" - {
+
+    act.like(renderView(onPageLoad) { implicit app => implicit request =>
+      injected[YesNoPageView]
+        .apply(form(injected[YesNoPageFormProvider]), viewModel(srn, assetIndex, disposalIndex, NormalMode))
+    })
+
+    act.like(renderPrePopView(onPageLoad, AssetSaleIndependentValuationPage(srn, assetIndex, disposalIndex), true) {
+      implicit app => implicit request =>
+        injected[YesNoPageView]
+          .apply(
+            form(injected[YesNoPageFormProvider]).fill(true),
+            viewModel(srn, assetIndex, disposalIndex, NormalMode)
+          )
+    })
+
+    act.like(redirectNextPage(onSubmit, "value" -> "true"))
+    act.like(redirectNextPage(onSubmit, "value" -> "false"))
+
+    act.like(journeyRecoveryPage(onPageLoad).updateName("onPageLoad" + _))
+
+    act.like(saveAndContinue(onSubmit, "value" -> "true"))
+
+    act.like(invalidForm(onSubmit))
+    act.like(journeyRecoveryPage(onSubmit).updateName("onSubmit" + _))
+  }
+}
