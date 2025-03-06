77aa614120225d25af672f66f5e44a20103dbff7
narendrapaduchuri <narendra.paduchuri@digital.hmrc.gov.uk>
2025-03-06 11:51:56 +0000

    PSR-1896 upgraded bootstrap version to 9.11.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.7.0"
-  private val hmrcMongoVersion = "2.3.0"
+  private val bootstrapVersion = "9.11.0"
+  private val hmrcMongoVersion = "2.5.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "11.8.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "11.11.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.2.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"        %% "tax-year"                               % "5.0.0",
     "uk.gov.hmrc"        %% "domain-play-30"                         % "10.0.0",
-    "uk.gov.hmrc"        %% "crypto-json-play-30"                    % "8.1.0",
+    "uk.gov.hmrc"        %% "crypto-json-play-30"                    % "8.2.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "org.typelevel"      %% "cats-core"                              % "2.12.0",
     "eu.timepit"         %% "refined"                                % "0.11.2",
