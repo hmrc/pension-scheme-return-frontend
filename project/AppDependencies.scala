586bbeacb4e75dcf7afb26b2f898189746a596ea
MadStu <737342+MadStu@users.noreply.github.com>
2025-03-19 10:30:09 +0000

    [PSR-1698] Character Count Fix
    .
    
    Update Dependencies
    
    Upgrade Scala and SBT versions
    
    Uddate Plugins
    
    Remove mappings edit

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,7 +3,7 @@ import sbt.*
 object AppDependencies {
 
   private val bootstrapVersion = "9.11.0"
-  private val hmrcMongoVersion = "2.5.0"
+  private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
