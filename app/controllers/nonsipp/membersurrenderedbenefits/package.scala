d2c86ed29dd8b16e9a799b8b1ee9bd7db339be14
Jamal Osman <7947206+jamalosman@users.noreply.github.com>
2025-06-11 12:07:04 +0100

    PSR-1864: fix regexes

--- app/controllers/nonsipp/membersurrenderedbenefits/package.scala
+++ app/controllers/nonsipp/membersurrenderedbenefits/package.scala
@@ -32,7 +32,7 @@ package object membersurrenderedbenefits {
 
     val isCyaPage: Boolean = {
       val pattern =
-        s"^\\/pension-scheme-return\\/[^\\/]+\\/(change-check-answers|check-answers)-surrender-benefits\\/(?!.*\\/\\d+\\/\\d+$$).*"
+        s"^\\/pension-scheme-return\\/[^\\/]+\\/(change-check-answers|check-answers)-surrender-benefits(?!.*\\/\\d+\\/\\d+$$).*"
 
       call.url.matches(pattern)
     }
