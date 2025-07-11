51f54c365d8cff1e27911cfacafe2bca1f6df5ef
kberec <26416940+kberec@users.noreply.github.com>
2025-07-11 12:44:08 +0100

    PSR-2021 Sorting by last name

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,12 +2,12 @@ import sbt.*
 
 object AppDependencies {
 
-  private val  bootstrapVersion = "9.13.0"
+  private val  bootstrapVersion = "9.14.0"
   private val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.6.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.7.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"        %% "tax-year"                               % "6.0.0",
