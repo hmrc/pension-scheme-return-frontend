b6a629ada182424e877b042c08dfa02ca2620258
MadStu <737342+MadStu@users.noreply.github.com>
2025-10-10 09:25:22 +0100

    [PSR-2144] Upgrade Dependencies
    .

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,17 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  private val  bootstrapVersion = "10.1.0"
-  private val hmrcMongoVersion = "2.7.0"
+  private val  bootstrapVersion = "10.2.0"
+  private val hmrcMongoVersion = "2.10.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.11.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.17.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"        %% "tax-year"                               % "6.0.0",
     "uk.gov.hmrc"        %% "domain-play-30"                         % "13.0.0",
-    "uk.gov.hmrc"        %% "crypto-json-play-30"                    % "8.3.0",
+    "uk.gov.hmrc"        %% "crypto-json-play-30"                    % "8.4.0",
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                     % hmrcMongoVersion,
     "org.typelevel"      %% "cats-core"                              % "2.13.0",
     "eu.timepit"         %% "refined"                                % "0.11.3",
