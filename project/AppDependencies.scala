875a5b13a2e78b25f01d109b15cb7b704224b0fb
MadStu <737342+MadStu@users.noreply.github.com>
2025-10-24 09:33:45 +0100

    [PSR-2147] Update country code to match ETMP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.18.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.19.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"        %% "tax-year"                               % "6.0.0",
