a073815d1ea755ea914232e656b8fa3e25969b94
MadStu <737342+MadStu@users.noreply.github.com>
2025-11-04 10:30:38 +0000

    [PSR-2150] Migrate frontends to new navigation component
    .

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val  bootstrapVersion = "10.3.0"
+  private val  bootstrapVersion = "10.4.0"
   private val hmrcMongoVersion = "2.10.0"
   private val pekkoVersion = "1.2.1"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.19.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.20.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.3.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"        %% "tax-year"                               % "6.0.0",
