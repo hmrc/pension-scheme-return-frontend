32bfa0c35114aab6b091e815f101f76b99dc8137
MadStu <737342+MadStu@users.noreply.github.com>
2025-12-03 14:10:02 +0000

    [PSR-2156] Update Dependencies non-SIPP FE

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import sbt.*
 object AppDependencies {
 
   private val  bootstrapVersion = "10.4.0"
-  private val hmrcMongoVersion = "2.10.0"
-  private val pekkoVersion = "1.2.1"
+  private val hmrcMongoVersion = "2.11.0"
+  private val pekkoVersion = "1.3.0"
 
   private val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.20.0",
-    "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.3.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"             % "12.22.0",
+    "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30"  % "3.4.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"             % bootstrapVersion,
     "uk.gov.hmrc"        %% "tax-year"                               % "6.0.0",
     "uk.gov.hmrc"        %% "domain-play-30"                         % "13.0.0",
