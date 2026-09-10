b9282fef4970ab8c6475b2236468c85bb1a3d456
andrewbrown22 <45789050+andrewbrown22@users.noreply.github.com>
2026-09-10 11:44:57 +0100

    DDCNL-12350 added .trim to buildHeaders to remove leading/trailing whitespace

--- it/test/connectors/PsrConnectorSpec.scala
+++ it/test/connectors/PsrConnectorSpec.scala
@@ -143,6 +143,35 @@ class PsrConnectorSpec extends BaseConnectorSpec with CommonTestValues {
       result mustBe Some(minimalSubmissionData)
     }
 
+    "return standard PSR when scheme name contains whitespace at the start/end" in runningApplication { implicit app =>
+      stubGet(
+        getStandardPsrDetailsUrl,
+        Map(
+          "periodStartDate" -> commonStartDate,
+          "psrVersion" -> commonVersion
+        ),
+        ok(Json.stringify(minimalSubmissionJson))
+          .withHeader("Content-Type", "application/json")
+          .withHeader("srn", "S0000000042")
+      )
+
+      val result =
+        connector
+          .getStandardPsrDetails(
+            pstr = commonPstr,
+            optFbNumber = None,
+            optPeriodStartDate = Some(commonStartDate),
+            optPsrVersion = Some(commonVersion),
+            fallBackCall = commonFallbackCall,
+            userName = commonUserName,
+            schemeName = " schemeNameWithSpaces ",
+            srn = Srn(commonSrn).get
+          )
+          .futureValue
+
+      result mustBe Some(minimalSubmissionData)
+    }
+
     "return standard Psr for fb number" in runningApplication { implicit app =>
       stubGet(
         getStandardPsrDetailsUrl,
